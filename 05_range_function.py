b6c2d2ec40f162bb4e573044d1f4c4c0ec7ea44d
sahil78272026 <sahil.garg78272026@gmail.com>
2022-04-20 06:13:40 +0530

    Add files via upload

---
+++ 05_range_function.py
@@ -0,0 +1,13 @@
+# range used to print sequence of numbers
+for num in range(8):  # will print number from 0 to 7
+    print(num)
+
+print ("\n")
+
+for num2 in range(2,8): # will print number from 2 to 7 , 
+    print(num2)
+
+print ("\n")
+
+for num3 in range(1,8,2):  # 2 is step size , will print numbers from 1 to 7 and skip two numbers eg, 1,3,5,7
+    print(num3)
